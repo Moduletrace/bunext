8d12329f01f61c22439543d65f1782579b53f3dc
Benjamin Toby <devops@tben.me>
2026-08-13 05:39:14 +0100

    Add perpetual watcher reload in dev

--- dist/functions/server/start-server.js
+++ dist/functions/server/start-server.js
@@ -1,10 +1,21 @@
 import _ from "lodash";
 import { log } from "../../utils/log";
 import serverParamsGen from "./server-params-gen";
+import isDevelopment from "../../utils/is-development";
+import watcherEsbuildCTX from "./watcher-esbuild-ctx";
 export default async function startServer() {
     const serverParams = await serverParamsGen();
     const server = Bun.serve(serverParams);
+    const is_dev = isDevelopment();
     global.BUNEXT_SERVER = server;
     log.server(`http://${server.hostname}:${server.port}`);
+    if (is_dev) {
+        setInterval(() => {
+            if (global.BUNEXT_PAGES_SRC_WATCHER) {
+                global.BUNEXT_PAGES_SRC_WATCHER.close();
+                watcherEsbuildCTX();
+            }
+        }, 5000);
+    }
     return server;
 }
